f9241119f7bcb6959489e61e0220e48a28d91257
geneologia5 <geneologia5@gmail.com>
2026-05-07 20:23:16 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,8 @@
-
+export default function Home() {
+  return (
+    <main style={{ padding: 20, fontFamily: "sans-serif" }}>
+      <h1>Arquivo Paroquial de Rabo de Peixe</h1>
+      <p>Site a funcionar ✔</p>
+    </main>
+  )
+}
